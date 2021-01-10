5903077889b16f9c1cf048b8bb011552a56456da
brichtom <56296772+brichtom@users.noreply.github.com>
2021-01-10 11:53:55 +0100

    třída kniha, její constructor

---
+++ app.js
@@ -0,0 +1,13 @@
+
+// Třída knihovna, reprezentuje knihovnu
+class Kniha {
+    constructor(id, autor, nazev, zanr, pozn) {
+        this.id = id;
+        this.autor = autor;
+        this.nazev = nazev;
+        this.zanr = zanr;
+        this.pozn = pozn;
+    }
+}
+
+
